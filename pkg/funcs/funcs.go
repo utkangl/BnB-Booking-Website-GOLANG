e6d959ce58c3aeba6164839007df7421a06aac4b
utkan <utkngl@gmail.com>
2023-01-30 04:55:49 +0300

    cached the visited templates using map

--- pkg/funcs/funcs.go
+++ pkg/funcs/funcs.go
@@ -3,6 +3,7 @@ package funcs
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -16,13 +17,48 @@ func AboutPage(Res http.ResponseWriter, Req *http.Request) {
 	RenderTmpl(Res, "aboutPage.tmpl")
 }
 
-func RenderTmpl(Res http.ResponseWriter, tmpl string) {
+var tmplCache = make(map[string]*template.Template) // this map will store the datas thar parsedTemplate function throws (and it's type is *template.Teamplate)
+// and the key to look up  to data is going to be a string
 
-	parsedTemplate, _ := template.ParseFiles("./Templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(Res, nil)
+func RenderTmpl(Res http.ResponseWriter, t string) {
+
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := tmplCache[t] // return true if we already have the template in the cache
+
+	if !inMap { // if inMap is false
+		log.Println("New template hes been created")
+		err = createTemplate(t) // creating the template with the string that we take as parameter of the function, and assigning variable err to possible returning error
+		if err != nil {
+			log.Println(err)
+		}
+
+	} else {
+		log.Println("This template is already in the cache and on use")
+	}
+
+	tmpl = tmplCache[t] // t is the name of the template, we are assigning the template to a variable called tmpl
+	err = tmpl.Execute(Res, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplate(t string) error {
+
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t), // assigning the parameter t as the name of the template
+		"./templates/base.layout.tmpl",   // setting the layout
+	}
+
+	tmpl, err := template.ParseFiles(templates...) // sending the string in the slice of string called templates to function ParseFiles,
+	// and assigning returns template and error as tmpl and err
 
 	if err != nil {
-		fmt.Println("Error occured while parsing template", err)
-		return
+		return err // return error if there is any
 	}
+
+	tmplCache[t] = tmpl //add template to our cache of templates
+	return nil
 }
