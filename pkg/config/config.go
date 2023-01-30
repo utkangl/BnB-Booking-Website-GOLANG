24ecec3afd10af0b828c7318f00abdb58ce3ded8
utkan <utkngl@gmail.com>
2023-01-31 02:32:48 +0300

    new comment lines added

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,8 @@ package config
 
 import "html/template"
 
-// this struct holds the application config
+// this struct holds the application config, config contains the template cache
+// so we dont need to load the cache every time we visit the template
 type AppConfig struct {
 	TemplateCache map[string]*template.Template
 }
