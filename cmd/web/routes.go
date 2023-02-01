f75b3e12dfabc883282ec4ac8ed718d523e85179
utkan <utkngl@gmail.com>
2023-02-01 03:52:02 +0300

    images added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -21,5 +21,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.HomePage)
 	mux.Get("/about", handlers.Repo.AboutPage)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
